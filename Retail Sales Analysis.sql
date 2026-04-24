ec27d1488b0e766e7be2dcd33ed07243d72e9fb6
Alvy Francis Piquero <alvyfrancispiquero@gmail.com>
2026-04-24 17:55:15 +0800

    Clean retail sales data and perform initial data exploration

--- Retail Sales Analysis.sql
+++ Retail Sales Analysis.sql
@@ -1,28 +1,85 @@
--- SQL Retail Sales Analysis
+CREATE DATABASE p1_retail_db;
 
--- Create TABLE
-DROP TABLE IF EXISTS retail_sales;
 CREATE TABLE retail_sales
-			(
-				transactions_id INT PRIMARY KEY,
-				sale_date DATE,	
-				sale_time TIME,	
-				customer_id	INT,
-				gender VARCHAR(15),	
-				age INT,	
-				category VARCHAR(15),	
-				quantiy INT,
-				price_per_unit FLOAT,	
-				cogs FLOAT,	
-				total_sale FLOAT
-			);
+(
+    transactions_id INT PRIMARY KEY,
+    sale_date DATE,	
+    sale_time TIME,
+    customer_id INT,	
+    gender VARCHAR(15),
+    age INT,
+    category VARCHAR(15),
+    quantity INT,
+    price_per_unit FLOAT,	
+    cogs FLOAT,
+    total_sale FLOAT
+);
 
 SELECT * FROM retail_sales
 LIMIT 10
 
 SELECT 
-	COUNT (*) 
+	COUNT(*) 
 FROM retail_sales
 
---
+-- Data Cleaning 
+SELECT * FROM retail_sales
+WHERE transactions_id is NULL
 
+SELECT * FROM retail_sales
+WHERE sale_date is NULL
+
+SELECT * FROM retail_sales
+WHERE sale_time is NULL
+
+SELECT * FROM retail_sales
+WHERE 
+	transactions_id is NULL
+	OR
+	sale_date is NULL
+	OR
+	sale_time is NULL
+	OR
+	gender is NULL
+	OR
+	category is NULL
+	OR 
+	quantity is NULL
+	OR
+	cogs is NULL
+	OR
+	total_sale is NULL;
+
+---
+DELETE FROM retail_sales
+WHERE 
+	transactions_id is NULL
+	OR
+	sale_date is NULL
+	OR
+	sale_time is NULL
+	OR
+	gender is NULL
+	OR
+	category is NULL
+	OR 
+	quantity is NULL
+	OR
+	cogs is NULL
+	OR
+	total_sale is NULL;
+
+-- Data Exploration
+
+-- How many sales?
+SELECT COUNT (*) as total_sale FROM retail_sales
+
+-- How many unique customers?
+SELECT COUNT (DISTINCT customer_id) as total_sale FROM retail_sales
+
+-- How many unique categories?
+SELECT COUNT (DISTINCT category) as total_sale FROM retail_sales
+-- Show unique categories
+SELECT DISTINCT category FROM retail_sales
+
+-- Data Analysis & Business Key Problems & Answers
